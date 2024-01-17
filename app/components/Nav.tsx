fe15c1ef5746c38658d6b00171f6777f95283449
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-01-17 22:45:33 +0200

    finished nav and header responsiveness, mobile menu to follow

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+'use client';
+import { Squash as Hamburger, Squash } from 'hamburger-react';
+import { navLinks } from '../data';
 import '../styles/Nav.scss';
+import React from 'react';
 
 export default function Nav() {
 	return (
 		<nav>
-			<div className='nav-links max-width'>awe</div>
+			<div className='nav-body max-width'>
+				<ol>
+					{navLinks.links.map((item) => {
+						return (
+							<li key={item.id}>
+								<a href={item.link}>{item.name}</a>
+							</li>
+						);
+					})}
+				</ol>
+
+				<button>Make a Booking</button>
+			</div>
+			<div className='mobile-nav'>
+				<p>MENU</p>
+				<div>
+					<Squash size={25} color='#FFF' />
+				</div>
+			</div>
 		</nav>
 	);
 }
---
+++ app/data.ts
@@ -0,0 +1,29 @@
+export const navLinks = {
+	links: [
+		{
+			id: 0,
+			name: 'Home',
+			link: '#Home',
+		},
+		{
+			id: 1,
+			name: 'About',
+			link: '#about',
+		},
+		{
+			id: 2,
+			name: 'Service',
+			link: '#service',
+		},
+		{
+			id: 3,
+			name: 'Testimonials',
+			link: '#testimonials',
+		},
+		{
+			id: 4,
+			name: 'Contact',
+			link: '#contact',
+		},
+	],
+};
