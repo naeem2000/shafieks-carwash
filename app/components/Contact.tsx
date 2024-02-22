5774c9cab5b58331b0f15136cb9a7e4b1dc2a086
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-02-22 22:54:00 +0200

    touch ups contact, reviews and added map and images

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -40,6 +40,12 @@ export default function Contact() {
 						</p>
 					</div>
 				</div>
+				<iframe
+					src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.9214639958554!2d18.487106674901337!3d-34.04588562825141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc43aefee8827d%3A0xd1cc0122e86e772e!2sShafiek&#39;s%20Car%20Wash!5e0!3m2!1sen!2sza!4v1708632621874!5m2!1sen!2sza'
+					allowFullScreen
+					loading='lazy'
+					referrerPolicy='no-referrer-when-downgrade'
+				></iframe>
 			</div>
 			<div className='form-section'>
 				<p>Request for a car wash</p>
