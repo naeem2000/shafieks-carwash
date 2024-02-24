15e2f245523ca26c7f223bd5a64cd71736f4f66a
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-02-24 12:27:25 +0200

    removed meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import Head from 'next/head';
 import './globals.scss';
 
 export const metadata: Metadata = {
-	description: `Your local favourte car wash.`,
+	title: `Shafiek's Carwash`,
+	description: `Your local favourite.`,
 };
 
 export default function RootLayout({
@@ -13,13 +13,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<Head>
-				<meta
-					name='google-site-verification'
-					content='qgCDozuB7q8avYtBYV_1gJpJ77rNBVS8zEmmYdamFBI'
-				/>
-				<title>Shafiek's Carwash</title>
-			</Head>
 			<body>{children}</body>
 		</html>
 	);
